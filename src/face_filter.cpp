54900f7c666992e279f736b5ae6f7103abf113bc
Preda Alexandru <alexpreda95@yahoo.com>
2018-05-23 19:20:10 +0300

    Face filter update

--- src/face_filter.cpp
+++ src/face_filter.cpp
@@ -8,91 +8,54 @@
 using namespace std;
 using namespace cv;
 
-/** Function Headers */
-void detectAndDisplay( Mat imagine );
-
-/** Global variables */
-//CascadeClassifier clasificator_fata("C:/opencv/sources/data/haarcascades/haarcascade_frontalface_alt.xml");
-//CascadeClassifier clasificator_ochi("C:/opencv/sources/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml");
-String face_cascade_name = "C:/opencv/sources/data/haarcascades/haarcascade_frontalface_alt.xml";
-String eyes_cascade_name = "C:/opencv/sources/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml";
-CascadeClassifier face_cascade;
-CascadeClassifier eyes_cascade;
-String window_name = "Capture - Face detection";
 
 /** @function main */
-int main( void )
+int main()
 {
-    VideoCapture capture;
-    Mat imagine;
-
-    //-- 1. Load the cascades
-    if( !face_cascade.load( face_cascade_name ) )
-	{ 
-		printf("--(!)Error loading face cascade\n"); 
-		return -1; 
-	};
-    if( !eyes_cascade.load( eyes_cascade_name ) )
-	{ 
-		printf("--(!)Error loading eyes cascade\n"); 
-		return -1; 
-	};
-
-    //-- 2. Read the video stream
-    capture.open( -1 );
-    if ( ! capture.isOpened() ) 
-	{ 
-		printf("--(!)Error opening video capture\n"); 
-		return -1; 
+	CascadeClassifier face_cascade, eye_cascade;
+	if (!face_cascade.load("C:/opencv/sources/data/haarcascades/haarcascade_frontalface_alt2.xml")) {
+		printf("Error loading cascade file for face");
+		return 1;
 	}
-
-    while (  capture.read(imagine) )
-    {
-        if(imagine.empty() )
-        {
-            printf(" --(!) No captured frame -- Break!");
-            break;
-        }
-
-        //-- 3. Apply the classifier to the frame
-        detectAndDisplay(imagine);
-
-        int c = waitKey(1);
-        if( (char)c == 27 ) { break; } // escape
-    }
-    return 0;
-}
-
-/** @function detectAndDisplay */
-void detectAndDisplay( Mat imagine)
-{
-    std::vector<Rect> fete;
-    
-
-    cvtColor(imagine, imagine, CV_BGR2GRAY);
-    equalizeHist(imagine, imagine);
-
-    //-- Detect faces
-    face_cascade.detectMultiScale(imagine, fete);
-
-    for( size_t i = 0; i < fete.size(); i++ )
-    {
-        Point center(fete[i].x + fete[i].width/2, fete[i].y + fete[i].height/2 );
-        ellipse( imagine, center, Size(fete[i].width/2, fete[i].height/2), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-
-        Mat faceROI = imagine(fete[i] );
-        std::vector<Rect> eyes;
-
-        //-- In each face, detect eyes
-        eyes_cascade.detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
-
-        for( size_t j = 0; j < eyes.size(); j++ )
-        {
-            Point eye_center(fete[i].x + eyes[j].x + eyes[j].width/2, fete[i].y + eyes[j].y + eyes[j].height/2 );
-            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-            circle( imagine, eye_center, radius, Scalar( 255, 0, 0 ), 4, 8, 0 );
-        }
-    }
-    //-- Show what you got
-    imshow( window_name, imagine);
+	if (!eye_cascade.load("C:/opencv/sources/data/haarcascades/haarcascade_eye.xml")) {
+		printf("Error loading cascade file for eye");
+		return 1;
+	}
+	VideoCapture capture(0); //-1, 0, 1 device id
+	if (!capture.isOpened())
+	{
+		printf("error to initialize camera");
+		return 1;
+	}
+	Mat cap_img, gray_img;
+	vector<Rect> faces, eyes;
+	while (1)
+	{
+		capture >> cap_img;
+		waitKey(10);
+		cvtColor(cap_img, gray_img, CV_BGR2GRAY);
+		cv::equalizeHist(gray_img, gray_img);
+		face_cascade.detectMultiScale(gray_img, faces, 1.1, 10, CV_HAAR_SCALE_IMAGE | CV_HAAR_DO_CANNY_PRUNING, cvSize(0, 0), cvSize(300, 300));
+		for (int i = 0; i < faces.size(); i++)
+		{
+			Point pt1(faces[i].x + faces[i].width, faces[i].y + faces[i].height);
+			Point pt2(faces[i].x, faces[i].y);
+			Mat faceROI = gray_img(faces[i]);
+			eye_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(30, 30));
+			for (size_t j = 0; j< eyes.size(); j++)
+			{
+				//Point center(faces[i].x+eyes[j].x+eyes[j].width*0.5, faces[i].y+eyes[j].y+eyes[j].height*0.5);
+				Point center(faces[i].x + eyes[j].x + eyes[j].width*0.5, faces[i].y + eyes[j].y + eyes[j].height*0.5);
+				int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
+				circle(cap_img, center, radius, Scalar(255, 0, 0), 2, 8, 0);
+			}
+			rectangle(cap_img, pt1, pt2, cvScalar(0, 255, 0), 2, 8, 0);
+		}
+		imshow("Result", cap_img);
+		waitKey(3);
+		char c = waitKey(3);
+		if (c == 27)
+			break;
+	}
+	return 0;
 }
